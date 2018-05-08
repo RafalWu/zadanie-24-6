905bf7093d8046872ca63ed10b4e5f747136a36e
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-08 12:55:36 +0200

    Modify PlayersList

---
+++ scorekeeper/src/components/PlayersList/PlayersList.test.js
@@ -0,0 +1,23 @@
+import PlayersList from './PlayersList';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  shallow(<PlayersList players={[]} />);
+});
+
+it('renders correct number of players', () => {
+	const players = [
+		{
+			name: 'Kunegunda',
+			score: 5
+		},
+		{
+			name: 'Antoś',
+			score: 0
+		}
+	]
+	const playerComponent = shallow(<PlayersList players={players} />);
+	const expectedPlayersNumber = playerComponent.find('li').length;
+	expect(expectedPlayersNumber).toEqual(2);
+});
