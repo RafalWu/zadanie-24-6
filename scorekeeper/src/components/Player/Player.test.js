fdd3c5da1f151aaae7871c00bc2d749ddfa9b307
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-08 12:24:51 +0200

    Add and modify Player

--- scorekeeper/src/components/Player/Player.test.js
+++ scorekeeper/src/components/Player/Player.test.js
@@ -14,3 +14,30 @@ it('renders correct name', () => {
 
   expect(playerNameRendered).toEqual(playerNamePassed);
 });
+
+it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
+  const mockedOnPlayerScoreChange = jest.fn();
+  const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+  const plusButton = playerComponent.find('.Player__button');
+
+  plusButton.simulate('click');
+
+  expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
+});
+
+
+/*
+it('should call onPlayerScoreChange with -1 when minus button is clicked', () => {
+  const mockedOnPlayerScoreChange = jest.fn();
+  const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+  const plusButton = playerComponent.find('.Player__button');
+
+  minusButton.simulate('click');
+
+  expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
+});
+*/
+
+
