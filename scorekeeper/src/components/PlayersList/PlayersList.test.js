0bed9b41b22c2d3189f116ff9d5fee8a722f1027
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-08 13:12:49 +0200

    Modify PL 2

--- scorekeeper/src/components/PlayersList/PlayersList.test.js
+++ scorekeeper/src/components/PlayersList/PlayersList.test.js
@@ -24,3 +24,22 @@ it('renders correct number of players', () => {
 	const expectedPlayersNumber = playerComponent.find(Player).length;
 	expect(expectedPlayersNumber).toEqual(2);
 });
+// finish name
+it('', () => {
+	const players = [
+		{
+			name: 'Kunegunda',
+			score: 5
+		},
+		{
+			name: 'Antoś',
+			score: 0
+		}
+	]
+	const mockedOnScoreUpdate = jest.fn();
+	const playerComponent = shallow(<PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+	const firstPlayer = playerComponent.find(Player).first();
+	const onPlayerScoreChange = firstPlayer.prop('onPlayerScoreChange');
+	onPlayerScoreChange(10);
+	expect(mockedOnScoreUpdate).toBeCalledWith(0, 10);
+});
