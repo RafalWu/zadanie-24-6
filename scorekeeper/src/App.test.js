c66d3e6f263207bcb24ca4e0fe76ba84b57a14a5
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-09 16:28:15 +0200

    Add AddPlayer

--- scorekeeper/src/App.test.js
+++ scorekeeper/src/App.test.js
@@ -5,3 +5,29 @@ import App from './App';
 it('renders without crashing', () => {
   shallow(<App />);
 });
+
+it('should update player score', () => {
+	players = [
+		{
+         name: 'Kunegunda',
+         score: 5,
+       },
+       {
+         name: 'Antoś',
+         score: 0,
+       }
+	]
+	shallow(<App />);
+	appComponent.setState({ players });
+	const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+	onScoreUpdate(0, 5);
+	const playersAfterUpdate = appComponent.state('players');
+	//alternatywnie appComponent.state.players
+	playersAfterUpdate[0].score
+	//NAPISANIE ASERCJI!!!!!!!
+}
+
+
+
+
+
